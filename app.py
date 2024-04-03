e1e2a7d56d145dbd6387cbffc39cb5b4683c01ed
AgentHaswing <156608097+AgentHaswing@users.noreply.github.com>
2024-04-03 08:46:34 -0400

    Update app.py
    Setting up app.py file

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from transformers import pipeline
 import gradio as gr
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
+# Load the model and tokenizer
+model = AutoModelForCausalLM.from_pretrained("ai21labs/Jamba-v0.1", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("ai21labs/Jamba-v0.1")
 
-model = pipeline(
-    "summarization",
-)
+def generate_text(prompt, max_new_tokens=216):
+    # Tokenize the prompt
+    input_ids = tokenizer(prompt, return_tensors='pt').to(model.device)["input_ids"]
 
-def predict(prompt):
-    summary = model(prompt)[0]["summary_text"]
-    return summary
+    # Generate text
+    outputs = model.generate(input_ids, max_new_tokens=max_new_tokens)
 
-with gr.Blocks() as demo:
-    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict,input=textbox,)(outputs='text')
+    # Decode the generated text
+    generated_text = tokenizer.batch_decode(outputs)
 
-demo.launch()
+    return generated_text
+
+# Example usage
+prompt = "In the recent Super Bowl LVIII,"
+generated_text = generate_text(prompt)
+print(generated_text)
+
+def greet(name):
+    return "Hello " + name + "!!"
+
+iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface.launch()
